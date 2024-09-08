c3ca27806e1cf275d71dcaf03f17096fe26b63e4
your-github-username <dheeraj.badalsharma@gmail.com>
2024-09-08 14:30:23 +0530

    abc

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
 import './index.css'
 import Dashboard from './pages/Dashboard/Dashboard'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <App /> */}
     <Dashboard/>
   </StrictMode>,
 )
